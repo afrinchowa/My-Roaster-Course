4fa361803f8c3597b88d810b4f24e107f294f1f1
Moriom <soya35-850@diu.edu.bd>
2023-09-16 04:34:00 +0600

    All Card added with their resonable description from json

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
-import Cart from "./components/Cart/Cart";
+
 
 function App() {
   return (
     <>
       <h1 className="text-6xl text-center font-bold  mb-10"> Course Registration </h1>
-      <div className="md:flex">
+      
         <Home></Home>
-        <Cart></Cart>
-      </div>
+      
     </>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,82 @@
 /* eslint-disable no-unused-vars */
+// import PropTypes from "prop-types";
+
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import "./Home.css";
-import Course from "../Course/Course";
+
 const Home = () => {
   const [courses, setCourses] = useState([]);
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [remaining, setRemaining] = useState(0);
+  const [totalCredit, setTotalCredit] = useState(0);
   useEffect(() => {
-    fetch("courses.json")
+    fetch("./courses.json")
       .then((res) => res.json())
       .then((data) => setCourses(data));
   }, []);
 
-  return (
-    <div className="md:w-3/4">
-      <h1>Courses:{courses.length}</h1>
-      {
-        courses.map(course => <Course key={course.price}
-        course={course}></Course>)
+  const handleSelectCourse = (course) => {
+    const isExist = selectedCourses.find((item) => item.price === course.price);
+    let count = course.credit_hour;
+
+    if (isExist) {
+      alert("Already buy");
+    } else {
+      selectedCourses.forEach((item) => {
+        count += item.credit_hour;
+      });
+
+      const totalRemaining = 20 - count;
+
+      if (count > 20) {
+        return alert(" OOPS!!! Sorry!!! Max Credit Reached");
+      }else{
+        setTotalCredit(count);
+        setRemaining(totalRemaining);
+        setSelectedCourses([...selectedCourses, course]);
       }
+    
+    }
+  };
+
+  return (
+    <div className="container ">
+      <div className="home-container">
+        <div className="card-container ">
+          {courses.map((course) => (
+            <div key={course.price} className="card shadow-2xl">
+              <img src={course.cover} alt="" />
+              <h2 className="text-base font-semibold text-center">
+                {course.title}
+              </h2>
+              <p className="text-sm font-normal">{course.description}</p>
+              <div className="flex justify-around">
+                <small>Price:{course.price}</small>
+                <small>Credit:{course.credit_hour}</small>
+              </div>
+              <button
+                onClick={() => handleSelectCourse(course)}
+                className="bg-blue-500 w-full   hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Select
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="cart shadow-2xl w-auto h-96 rounded-lg">
+          <Cart
+            selectedCourses={selectedCourses}
+            remaining={remaining}
+            totalCredit={totalCredit}
+          ></Cart>
+        </div>
+      </div>
     </div>
   );
 };
+// Home.PropTypes = {
+//   course: PropTypes.object,
+// };
 
 export default Home;
